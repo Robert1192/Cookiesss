f4f20f4aee149a63493da21cc3f1dd36cc91b94b
Junior2548 <pongsapat2548@gmail.com>
2024-09-23 14:40:52 +0200

    test2222

--- CookiesPr/MainWindow.xaml.cs
+++ CookiesPr/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace CookiesPr
             CookieMeter.Text = cookiecount.ToString();
         }
 
-      // test
+      // test2222
 
         private void SettingsIcon_Click(object sender, RoutedEventArgs e)
         {
