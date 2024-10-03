ad508512848b8ac26630ae4ca1334c80ceb46c1e
Robert1192 <robertapostol15@gmail.com>
2024-10-03 09:30:31 +0200

    Added Events

--- CookiesPr/MainWindow.xaml.cs
+++ CookiesPr/MainWindow.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System.Diagnostics.Eventing.Reader;
-using System.Net;
-using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace CookiesPr
@@ -21,15 +11,28 @@ namespace CookiesPr
         private int upgradeCost = 10; // Cost of each click power upgrade
         private int autoClickerCost = 100; // Cost of the auto clicker upgrade
         private DispatcherTimer autoClickerTimer; // Timer for the auto clicker
+        private DispatcherTimer randomEventTimer; // Timer for triggering random events
         private bool isAutoClickerActive = false; // Track whether the auto-clicker is active
         private int autoClickDuration = 10; // Auto-clicker will run for 10 seconds
         private int autoClickCounter = 0; // Counter for auto-click events
+        private Random random; // Random instance for event selection
 
         public MainWindow()
         {
             InitializeComponent();
+            random = new Random(); // Initialize random
             UpdateCookieMeter();
+
+            // Initialize the DispatcherTimer for the auto clicker
             autoClickerTimer = new DispatcherTimer();
+            autoClickerTimer.Interval = TimeSpan.FromSeconds(1); // Set interval to 1 second
+            autoClickerTimer.Tick += AutoClick; // Hook up the event to simulate clicks
+
+            // Set up a timer to trigger random events every 30 seconds
+            randomEventTimer = new DispatcherTimer();
+            randomEventTimer.Interval = TimeSpan.FromSeconds(30);
+            randomEventTimer.Tick += TriggerRandomEvent; // Hook up random event trigger
+            randomEventTimer.Start(); // Start the random event timer
         }
 
         // Method to update the cookie meter display
@@ -51,7 +54,7 @@ namespace CookiesPr
                 cookies -= upgradeCost; // Deduct the cost
                 cookiesPerClick++; // Increase clicking power by 1
                 upgradeCost *= 2; // Increase the cost for the next upgrade
-                UpgradePowerClick.Content = $"ClickPower ({upgradeCost})";
+                UpgradePowerClick.Content = $"ClickPower ($ {upgradeCost})";
                 UpdateCookieMeter(); // Update the UI display
             }
             else
@@ -85,37 +88,77 @@ namespace CookiesPr
             // Set auto-clicker as active
             isAutoClickerActive = true;
             autoClickCounter = 0; // Reset the auto-click counter
-
-            // Initialize the DispatcherTimer to simulate clicks every second
-            autoClickerTimer = new DispatcherTimer();
-            autoClickerTimer.Interval = TimeSpan.FromSeconds(1); // Set interval to 1 second
-            autoClickerTimer.Tick += AutoClick; // Hook up the event to simulate clicks
-            autoClickerTimer.Start();
+            autoClickerTimer.Start(); // Start the auto-clicker timer
         }
-        private void AutoClick(object? sender, EventArgs e) // Allow nullable sender
+
+        private void AutoClick(object? sender, EventArgs e) // Mark sender as nullable
         {
             if (autoClickCounter < autoClickDuration)
             {
-                cookies += cookiesPerClick;
-                UpdateCookieMeter();
-                autoClickCounter++;
+                cookies += cookiesPerClick; // Simulate auto-click
+                UpdateCookieMeter(); // Update the UI display
+                autoClickCounter++; // Increment the auto-click counter
             }
             else
             {
-                autoClickerTimer.Stop();
-                isAutoClickerActive = false;
+                autoClickerTimer.Stop(); // Stop the timer when auto-click duration is reached
+                isAutoClickerActive = false; // Reset the auto-clicker state
             }
         }
+
+        // Random event trigger (either burn or overload cookies)
+        private void TriggerRandomEvent(object? sender, EventArgs e) // Mark sender as nullable
+        {
+            int eventChoice = random.Next(2); // Randomly choose between 0 (Burn) and 1 (Add)
+            if (eventChoice == 0)
+            {
+                BurnCookies(this, EventArgs.Empty); // Trigger the BurnCookies event
+            }
+            else
+            {
+                AddCookies(this, EventArgs.Empty); // Trigger the AddCookies event
+            }
+        }
+
+        // Button click to manually trigger BurnCookies
+        private void BurnCookies_Click(object sender, RoutedEventArgs e)
+        {
+            BurnCookies(this, EventArgs.Empty); // Manually trigger burn cookies
+        }
+
+        // Button click to manually trigger AddCookies
+        private void AddCookies_Click(object sender, RoutedEventArgs e)
+        {
+            AddCookies(this, EventArgs.Empty); // Manually trigger add cookies
+        }
+
+        // Event: Burn 25% of cookies
+        private void BurnCookies(object sender, EventArgs e)
+        {
+            // Ensure cookies do not go negative
+            if (cookies > 0)
+            {
+                cookies = (int)(cookies * 0.75); // Decrease by 25%
+                MessageBox.Show("Event: Cookie Burner! -25% Cookies Lost."); // Message shown in message box
+                UpdateCookieMeter();
+            }
+            else
+            {
+                MessageBox.Show("No cookies to burn!"); // Notify that there are no cookies to burn
+            }
+        }
+
+        // Event: Add 25% more cookies
+        private void AddCookies(object sender, EventArgs e)
+        {
+            cookies = (int)(cookies * 1.25); // Increase by 25%
+            MessageBox.Show("Event: Cookie Overload! +25% Cookies Added."); // Message shown in message box
+            UpdateCookieMeter();
+        }
+
         private void SettingsIcon_Click(object sender, RoutedEventArgs e)
         {
             // Logic for settings icon click (if implemented)
         }
-
-        private void UpgradeAutoClickSpeed_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
-
-
